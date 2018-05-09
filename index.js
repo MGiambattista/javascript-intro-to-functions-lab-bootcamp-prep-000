c767955c24d7d47ee803ed200edc4d5c845c35cd
Michael Giambattista <giamber33@gmail.com>
2018-05-09 16:33:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  var uppercase = "HELLO"
-  var lowercase = "hello"
   if (string === lowercase){
     return "I can't hear you!"
   }else if (string === uppercase) {
